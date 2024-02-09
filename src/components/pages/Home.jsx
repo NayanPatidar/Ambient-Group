ac3fdb09cd5200425d62d9f16496fa39af3ac57f
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-09 23:57:05 +0530

    Basic Files added

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TopPage = () => {
+  return (
+    <div className="main-container">
+        <div className=" flex flex-col">
+          <img 
+            src=""
+          />
+        </div>
+    </div>
+  );
+};
+
+export default TopPage;
