d929a91e922e578661c0f074163dd421e23412e3
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-12 22:35:13 +0530

    Aded App.jsx for Data Transfer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Properties from "./pages/Properties.jsx";
 
 export default function App() {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsMouseOver(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsMouseOver(false);
+  };
+
   return (
     <div className="background-container z-0">
-      <Properties />
+      <Properties
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home isMouseOver={isMouseOver} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
