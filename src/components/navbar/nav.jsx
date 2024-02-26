5dbd67b680875dba007650abc41a2a4eb7a63f14
Nayan Patidar <nayanpatidar28@gmail.com>
2024-02-26 17:34:35 +0530

    NavBar Responsiveness Added

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -12,15 +12,15 @@ const Navbar = () => {
 
   return (
     <div className="navigation-bar">
-      <div className="flex flex-row justify-between items-center mt-8 lg:ml-8 md:ml-8 ml-5 ">
-        <div className="text-white text-2xl font-thin pl-1 cursor-pointer items-center self-center mr-5">
+      <div className="flex flex-row justify-between items-center mt-8 ">
+        <div className="Title text-white font-thin pl-8 cursor-pointer items-center self-center mr-5">
           <span className=" font-bold" onClick={HomePage}>
             AMBIENT
           </span>{" "}
           GROUP
         </div>
 
-        <div className=" text-white mr-10">
+        <div className="NavSites text-white">
           <ul className="flex flex-row space-x-4  lg:gap-8 md:gap-8 gap-1 items-center">
             <li className="">
               <a
